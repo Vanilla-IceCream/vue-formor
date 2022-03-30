756aecd0016a2f2e4e457333da7cc671992281e6
Shyam-Chen <shyamchen1994@gmail.com>
2022-03-30 10:22:05 +0800

    18th Commit

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = (api) => {
   api.cache(true);
 
   return {
-    presets: ['@babel/preset-env'],
-    plugins: ['@babel/plugin-transform-runtime'],
+    // presets: ['@babel/preset-env'],
+    // plugins: ['@babel/plugin-transform-runtime'],
   };
 };
