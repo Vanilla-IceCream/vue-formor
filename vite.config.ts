3aca1477719f3587c3eef9108f142ea3ca3b2927
Shyam-Chen <shyamchen1994@gmail.com>
2022-10-03 14:58:36 +0800

    27th Commit

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/vue-formor.js'),
+      name: 'VueFormor',
+      // the proper extensions will be added
+      fileName: 'vue-formor'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue', 'message-interpolation'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+          'message-interpolation': 'MessageInterpolation'
+        }
+      }
+    }
+  }
+})
