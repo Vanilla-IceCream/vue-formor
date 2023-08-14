5551daa46d4c4581ce527da70c6f3c6fa2244b42
Shyam-Chen <shyamchen1994@gmail.com>
2023-08-14 13:43:08 +0800

    44th Commit

--- vite.config.ts
+++ vite.config.ts
@@ -11,11 +11,6 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-      },
     },
   },
   plugins: [vue(), dts()],
